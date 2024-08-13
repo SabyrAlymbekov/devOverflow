e93d2b502ff87d821cd4990889c61cd28a8e4f26
SabAl <alymbekovsabyr@gmail.com>
2024-08-13 11:03:22 +0600

    added auth

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+const SignIn = () => {
+    return (
+        <div>sign-in</div>
+    )
+}
+
+export default SignIn
---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const AskQuestion = () => {
+    return (
+        <div>ask question</div>
+    )
+}
+
+export default AskQuestion
